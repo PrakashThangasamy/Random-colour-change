4902a1ceb7637a6def28affb64befe83831e1e87
Prakash <98002602+SmashPrakash@users.noreply.github.com>
2023-02-05 10:24:43 +0530

    Add files via upload

---
+++ task7/script.js
@@ -0,0 +1,10 @@
+// const toggleButton = document.getElementById("toggleButton");
+// const box = document.getElementById("box");
+// toggleButton
+// .addEventListener("click", function () {
+// if (box.style.display === "none") {
+// box.style.display = "block";
+// } else {
+// box.style.display = "none";
+// }
+// });
